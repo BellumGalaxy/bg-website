dc108b7ee4f4e9cdb0a2743ce02d99ed68beab9f
Barba <patrickamc@protonmail.com>
2023-12-30 15:13:38 -0300

    footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center relative bg-bgcolor">
+      <p>© 2024 Barba. Todos os Direitos Reservados.</p>
+    </footer>
+  );
+}
+export default Footer
--- app/components/Parceiros.jsx
+++ app/components/Parceiros.jsx
@@ -11,28 +11,32 @@ const parceiros = [
 
 const Parceiros = () => {
   return (
-    <div
-      className="hero min-h-screen flex flex-col justify-start bg-bgcolor"
-      style={{
-        backgroundImage:
-          "linear-gradient(to top, rgba(255, 255, 255, 0) 0%, 90%, #194659 100%), url(./imagens/parceiros.jpg)",
-      }}
-    >
-      <div className="flex text-center mt-32">
-        <span className="text-[100px] font-bold text-bgvertical">
-          PARCEIROS
-        </span>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 px-4">
-        {parceiros.map((parceiro, index) => (
-          <div key={index} className="flex justify-center items-center">
-            <img
-              src={parceiro.img}
-              alt={`Parceiro ${index + 1}`}
-              className="object-contain h-48 w-48"
-            />
+    <div className="flex flex-col min-h-screen bg-bgcolor">
+      <div className="flex-1">
+        <div
+          className="hero min-h-screen flex flex-col justify-start"
+          style={{
+            backgroundImage:
+              "linear-gradient(to top, rgba(255, 255, 255, 0) 0%, 90%, #194659 100%), url(./imagens/parceiros.jpg)",
+          }}
+        >
+          <div className="flex text-center mt-32">
+            <span className="text-[100px] font-bold text-bgvertical">
+              PARCEIROS
+            </span>
           </div>
-        ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 px-4">
+            {parceiros.map((parceiro, index) => (
+              <div key={index} className="flex justify-center items-center">
+                <img
+                  src={parceiro.img}
+                  alt={`Parceiro ${index + 1}`}
+                  className="object-contain h-48 w-48"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
